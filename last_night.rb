fae4a69a149509b4c4c1982beace9270aeda033a
Emma A. A. Rünzel <emma@ruenzel.de>
2023-07-11 20:27:45 +0200

    Prep Flow Lecture

---
+++ last_night.rb
@@ -0,0 +1,13 @@
+# Start with a 5-minutes rehearsal on data types they know (String, Integer, Float, Booleans), and on variables and methods
+
+# String
+
+# Integer
+
+# Float
+
+# Booleans
+
+# Variables
+
+# Methods >>>>> Reusable Code
